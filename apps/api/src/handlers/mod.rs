5abc4430cc5dc3b213c1ac8981a995b15944e2a2
xuerzong <xuerzong@gmail.com>
2026-03-18 11:10:57 +0800

    feat: add dedicated embeddings handler

--- apps/api/src/handlers/mod.rs
+++ apps/api/src/handlers/mod.rs
@@ -1,10 +1,12 @@
 pub mod chat_completions;
+pub mod embeddings;
 pub mod health;
 pub mod messages;
 pub mod models;
 pub mod responses;
 
 pub use chat_completions::chat_completions_handler;
+pub use embeddings::embeddings_handler;
 pub use health::health_check_handler;
 pub use messages::messages_handler;
 pub use models::list_models_handler;
